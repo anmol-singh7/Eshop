b99aa7b8a2f78fe5b3924434e4b403842cc169f5
Anmol Singh <singhanmol0984@gmail.com>
2023-07-19 02:02:36 +0530

    take env out of config foldto to root folder

--- backend/app.js
+++ backend/app.js
@@ -5,10 +5,7 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-app.use(cors({
-  origin: 'http://localhost:3000',
-  credentials: true
-}));
+app.use(cors());
 
 app.use(express.json());
 app.use(cookieParser());
@@ -21,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 // config
 if (process.env.NODE_ENV !== "PRODUCTION") {
   require("dotenv").config({
-    path: "config/.env",
+    path: ".env",
   });
 }
 
